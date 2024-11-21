00af3299b101bf069053967a7d51ae4ffde36afc
Jason Gerard DeRose <jasonderose@gmail.com>
2024-11-21 11:53:31 -0700

    Bit O test cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@ impl<'a> Block<'a> {
 mod tests {
     use super::*;
 
-    const EXPECTED: &str = "e17e48f651b5d03585d26accca19bd39466a3aa46f7d499c4bd5a449eb2a0097";
+    const EXPECTED: &str = "8c055bbd86ce68355dbccdea130317563c638f482690eb7fac3f821e624061fc";
 
     fn new_expected() -> Hash {
         Hash::from_hex(EXPECTED).unwrap()
@@ -86,20 +86,15 @@ mod tests {
     fn new_valid_store() -> Vec<u8> {
         let mut store = Vec::with_capacity(BLOCK);
         store.extend_from_slice(new_expected().as_bytes());
-        store.extend_from_slice(&[2; PAYLOAD][..]);
-        store.extend_from_slice(&[3; DIGEST][..]);
+        store.extend_from_slice(&[2; SIGNATURE][..]);
+        store.extend_from_slice(&[3; PUBKEY][..]);
+        store.extend_from_slice(&[4; DIGEST][..]);  // NEXT_PUBKEY_HASH
+        store.extend_from_slice(&[5; DIGEST][..]);  // STATE_HASH
+        store.extend_from_slice(&[6; DIGEST][..]);  // PREVIOUS_HASH
         store
     }
 
     fn new_store() -> Vec<u8> {
-        let mut store: Vec<u8> = Vec::with_capacity(BLOCK);
-        store.extend_from_slice(&[1; DIGEST][..]);
-        store.extend_from_slice(&[2; PAYLOAD][..]);
-        store.extend_from_slice(&[3; DIGEST][..]);
-        store
-    }
-
-    fn new_store2() -> Vec<u8> {
         let mut store: Vec<u8> = Vec::with_capacity(BLOCK);
         store.extend_from_slice(&[1; DIGEST][..]);
         store.extend_from_slice(&[2; SIGNATURE][..]);
@@ -142,15 +137,12 @@ mod tests {
     fn block_as_hashable() {
         let store = new_store();
         let block = Block::new(&store[..]);
-        let mut expected = Vec::new();
-        expected.extend_from_slice(&[2; PAYLOAD][..]);
-        expected.extend_from_slice(&[3; DIGEST][..]);
-        assert_eq!(block.as_hashable(), &expected[..]);
+        assert_eq!(block.as_hashable(), &store[DIGEST..]);
     }
 
     #[test]
     fn block_as_signable() {
-        let store = new_store2();
+        let store = new_store();
         let block = Block::new(&store[..]);
         assert_eq!(block.as_signable(), &store[DIGEST + SIGNATURE..]);
     }
@@ -181,6 +173,6 @@ mod tests {
         let store = new_store();
         let block = Block::new(&store[..]);
         let hash = block.previous_hash();
-        assert_eq!(hash, Hash::from_bytes([3; DIGEST]));
+        assert_eq!(hash, Hash::from_bytes([6; DIGEST]));
     }
 }
