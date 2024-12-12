061570002eb63590d42072e129f05df16fce4aa8
Jason Gerard DeRose <jasonderose@gmail.com>
2024-12-12 11:38:17 -0700

    Start complete rework of SecretChain

--- src/secretchain.rs
+++ src/secretchain.rs
@@ -3,7 +3,7 @@
 use blake3::{keyed_hash, Hash, Hasher};
 use std::fs::File;
 use std::io::Result as IoResult;
-use std::io::{Read, Write};
+use std::io::{Read, Seek, Write};
 
 /*
 Steps to create a new chain:
@@ -37,6 +37,7 @@ fn derive(context: &str, secret: &[u8]) -> Hash {
 /// let seed = Seed::create(&initial_entropy);
 /// let seed = seed.advance(&new_entropy);
 /// ```
+#[derive(Debug, PartialEq)]
 pub struct Seed {
     pub secret: Hash,
     pub next_secret: Hash,
@@ -51,6 +52,14 @@ impl Seed {
             secret,
             next_secret,
         }
+    }
+
+    pub fn as_secret_bytes(&self) -> &[u8] {
+        self.secret.as_bytes()
+    }
+
+    pub fn as_next_secret_bytes(&self) -> &[u8] {
+        self.next_secret.as_bytes()
     }
 
     pub fn load(buf: &[u8; 64]) -> Self {
@@ -72,43 +81,20 @@ impl Seed {
     }
 }
 
+
 pub struct SecretChain {
     file: File,
-    pub key: Hash,
-    pub next_key: Option<Hash>,
 }
 
 impl SecretChain {
-    pub fn new(file: File, initial_entropy: &[u8; 32]) -> Self {
-        let mut h = Hasher::new_derive_key(SECRET_CHAIN_CONTEXT);
-        h.update(initial_entropy);
-        Self {
-            file,
-            key: h.finalize(),
-            next_key: None,
-        }
+    pub fn create(mut file: File, seed: &Seed) -> IoResult<Self> {
+        file.write_all(seed.as_secret_bytes())?;
+        file.write_all(seed.as_next_secret_bytes())?;
+        Ok(Self {file})
     }
 
-    pub fn advance(&mut self, new_entropy: &[u8; 32]) {
-        if self.next_key.is_some() {
-            panic!("Cannot call Chain.advance() when next_key is Some");
-        }
-        self.next_key = Some(keyed_hash(self.key.as_bytes(), new_entropy));
-    }
-
-    pub fn unadvance(&mut self) {
-        if self.next_key.take().is_none() {
-            panic!("Cannot call Chain.unadvance() when next_key is None");
-        }
-    }
-
-    pub fn commit(&mut self) -> IoResult<()> {
-        let key = self.next_key.take();
-        if key.is_none() {
-            panic!("Cannot call Chain.commit() when next_key is None");
-        }
-        self.key = key.unwrap();
-        self.file.write_all(self.key.as_bytes())
+    pub fn into_file(self) -> File {
+        self.file
     }
 }
 
@@ -118,12 +104,6 @@ mod tests {
     use blake3::hash;
     use std::collections::HashSet;
     use tempfile::tempfile;
-
-    fn new_sc() -> SecretChain {
-        let file = tempfile().unwrap();
-        let key = [69; 32];
-        SecretChain::new(file, &key)
-    }
 
     #[test]
     fn test_seed_new() {
@@ -188,54 +168,16 @@ mod tests {
     }
 
     #[test]
-    fn test_sc_new() {
+    fn test_sc_create() {
         let file = tempfile().unwrap();
-        let key = [69; 32];
-        let sc = SecretChain::new(file, &key);
-        assert_eq!(
-            sc.key,
-            Hash::from_hex("1f90fc1e2ad76220f1c4069018e0b48ecba090379aa6dc969b2d92a67fb05e49")
-                .unwrap()
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Cannot call Chain.advance() when next_key is Some")]
-    fn test_sc_advance_panic() {
-        let mut sc = new_sc();
-        sc.advance(&[42; 32]);
-        sc.advance(&[77; 32]);
-    }
-
-    #[test]
-    #[should_panic(expected = "Cannot call Chain.unadvance() when next_key is None")]
-    fn test_sc_unadvance_panic() {
-        let mut sc = new_sc();
-        sc.unadvance();
-    }
-
-    #[test]
-    #[should_panic(expected = "Cannot call Chain.commit() when next_key is None")]
-    fn test_sc_commit_panic() {
-        let mut sc = new_sc();
-        sc.commit().unwrap();
-    }
-
-    #[test]
-    fn test_sc_unadvance() {
-        let mut sc = new_sc();
-        sc.advance(&[42; 32]);
-        assert!(sc.next_key.is_some());
-        sc.unadvance();
-        assert!(sc.next_key.is_none());
-    }
-
-    #[test]
-    fn test_sc_commit() {
-        let mut sc = new_sc();
-        sc.advance(&[42; 32]);
-        assert!(sc.next_key.is_some());
-        assert!(sc.commit().is_ok());
-        assert!(sc.next_key.is_none());
+        let seed = Seed::create(&[42; 32]);
+        let result = SecretChain::create(file, &seed);
+        assert!(result.is_ok());
+        let mut file = result.unwrap().into_file();
+        file.rewind().unwrap();
+        let mut buf = [0; 64];
+        file.read_exact(&mut buf).unwrap();
+        let seed2 = Seed::load(&buf);
+        assert_eq!(seed, seed2);
     }
 }
