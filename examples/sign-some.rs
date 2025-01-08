b97449e8275cebace99dc5a217b2d1b6ffd8c8ed
Jason Gerard DeRose <jasonderose@gmail.com>
2025-01-08 07:11:45 -0700

    Clean up sign-some example

--- examples/sign-some.rs
+++ examples/sign-some.rs
@@ -1,17 +1,15 @@
+//! Create a new chain and some signatures.
+
 use blake3::Hash;
-use std::io::prelude::*;
 use tempfile;
-use zebrachain::always::*;
-use zebrachain::chain::{Chain, ChainStore};
+use zebrachain::chain::ChainStore;
 use zebrachain::ownedchain::SignerMajig;
-use zebrachain::pksign::{create_first_block, SigningChain};
-use zebrachain::secretchain::SecretChainStore;
 use zebrachain::secretseed::random_hash;
 
 fn build_state_hashes() -> Vec<Hash> {
     let count = 10000;
     let mut states = Vec::with_capacity(count);
-    for i in 0..count {
+    for _ in 0..count {
         states.push(random_hash());
     }
     states
@@ -40,29 +38,4 @@ fn main() {
             state_hash
         );
     }
-    /*
-        let chain_hash = chain.state.head.block_hash;
-        let mut chain = cs.open_chain(&chain_hash).unwrap();
-        chain.validate().unwrap();
-
-        let tmpdir2 = tempfile::TempDir::new().unwrap();
-        let scs = SecretChainStore::new(tmpdir2.path().to_path_buf());
-        let seed = Seed::auto_create();
-        let chain_hash = Hash::from_bytes([42; 32]);
-        let state_hash = Hash::from_bytes([69; 32]);
-        let mut secretchain = scs.create_chain(&chain_hash, seed, &state_hash).unwrap();
-        for state_hash in states {
-            let next = secretchain.auto_advance();
-            secretchain.commit(next, &state_hash).unwrap();
-        }
-    */
 }
-
-/*
-Create new chain:
-1. create seed
-2. construct first block in memory (sign and hash)
-3. use block hash hex to name chain file
-4. open file
-5. write first block
-*/
