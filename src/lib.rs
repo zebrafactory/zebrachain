5b85ebe6b3d7cc34c334f7db7162a13ea79d5de7
Jason Gerard DeRose <jasonderose@gmail.com>
2025-02-08 04:38:45 -0700

    Give a decent overview of ZebraChain in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-//! Quantum secure blockchain using [NIST post quantum standards][nist.gov].
+//! 🦓 🔗 ZebraChain: A futuristic cryptographic identity system.
+//!
+//! ZebraChain is a logged, quantum safe signing protocol designed to replace the long lived
+//! asymmetric key pairs used to sign software releases (and to sign other super important stuff).
+//!
+//! ZebraChain uses the [NIST post quantum standard][nist.gov] algorithm [Dilithium] alongside
+//! [ed25519] in a hybrid signing construction (as recommended by the Dilithium authors).
 //!
 //! [nist.gov]: https://www.nist.gov/news-events/news/2024/08/nist-releases-first-3-finalized-post-quantum-encryption-standards
+//! [Dilithium]: https://pq-crystals.org/dilithium/
+//! [ed25519]: https://ed25519.cr.yp.to/
 
 pub mod always;
 pub mod block;
