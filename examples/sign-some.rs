2fc31bd2a991bc4a03f90375012c12412c4a9b5c
Jason Gerard DeRose <jasonderose@gmail.com>
2025-03-05 12:03:46 -0700

    Cleanup up sign-some example, make output more friendly

--- examples/sign-some.rs
+++ examples/sign-some.rs
@@ -24,6 +24,7 @@ fn build_requests() -> Vec<SigningRequest> {
 }
 
 fn main() {
+    println!("Pre-generating {} random signing requests...", COUNT);
     let requests = build_requests();
     let tmpdir = tempfile::TempDir::new().unwrap();
     let root_secret = random_secret().unwrap();
@@ -31,61 +32,64 @@ fn main() {
     let initial_entropy = random_secret().unwrap();
     let mut chain = ocs.create_chain(&initial_entropy, &requests[0]).unwrap();
 
-    println!(
-        "{} {} {}",
-        chain.tail().chain_hash,
-        chain.tail().block_hash,
-        &requests[0].state_hash,
-    );
+    println!("Created new chain in directory {:?}", tmpdir.path());
 
+    println!("Signing remaning {} requests... ", COUNT - 1);
     for request in &requests[1..] {
         let new_entropy = random_secret().unwrap();
         chain.sign(&new_entropy, &request).unwrap();
-        println!(
-            "{} {} {}",
-            chain.tail().chain_hash,
-            chain.tail().block_hash,
-            request.state_hash
-        );
     }
-
     let chain_hash = chain.tail().chain_hash;
     let head = chain.head().clone();
     let tail = chain.tail().clone();
+    println!("Head: {}", head.block_hash);
+    println!("Tead: {}", tail.block_hash);
+    println!("Count: {}", chain.count());
 
     let filename = chain_filename(tmpdir.path(), &chain_hash);
-    println!("{:?}", filename);
     let file = open_for_append(&filename).unwrap();
+    println!("Opening chain and fully validating...");
     let chain = Chain::open(file, &chain_hash).unwrap();
+    println!("Iterating through chain and fully validating...");
     for result in &chain {
-        let state = result.unwrap();
-        println!("{}", state.block_hash);
+        let _state = result.unwrap();
+        //println!("{}", state.block_hash);
     }
+    println!("Removing public chain file {:?}", filename);
     ocs.store().remove_chain_file(&chain_hash).unwrap();
 
     let chain_secret = keyed_hash(root_secret.as_bytes(), chain_hash.as_bytes());
     let filename = secret_chain_filename(tmpdir.path(), &chain_hash);
     let file = open_for_append(&filename).unwrap();
+    println!("Opening secret chain and fully validating...");
     let secchain = SecretChain::open(file, chain_secret).unwrap();
 
+    println!("Iterating through secret chain and fully validating...");
     for result in &secchain {
-        let secblock = result.unwrap();
-        println!("state_hash: {}", secblock.request.state_hash);
+        let _secblock = result.unwrap();
+        //println!("state_hash: {}", secblock.request.state_hash);
     }
 
+    println!("Rebuilding public chain from secret chain...");
     let chain = ocs.secret_to_public(&secchain).unwrap();
     assert_eq!(&head, chain.head());
     assert_eq!(&tail, chain.tail());
 
+    println!("Opening chain and fully validating...");
     let chain = ocs.open_chain(&chain_hash).unwrap();
-    println!("{} {}", chain.tail().index, chain.tail().block_hash);
+    println!("Head: {}", chain.head().block_hash);
+    println!("Tead: {}", chain.tail().block_hash);
+    println!("Count: {}", chain.count());
     assert_eq!(chain.count(), requests.len() as u64);
     assert_eq!(&head, chain.head());
     assert_eq!(&tail, chain.tail());
 
     let checkpoint = CheckPoint::from_block_state(&tail);
+    println!("Resuming chain from a checkpoint and partially validating...");
     let chain = ocs.resume_chain(&checkpoint).unwrap();
-    println!("{} {}", chain.tail().index, chain.tail().block_hash);
+    println!("Head: {}", chain.head().block_hash);
+    println!("Tead: {}", chain.tail().block_hash);
+    println!("Count: {}", chain.count());
     assert_eq!(chain.count(), requests.len() as u64);
     assert_eq!(&head, chain.head());
     assert_eq!(&tail, chain.tail());
