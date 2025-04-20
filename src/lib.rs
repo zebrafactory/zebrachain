3ead495b8d244b8790c68d70df8b700143f836e1
Jason Gerard DeRose <jasonderose@gmail.com>
2025-04-20 17:54:03 -0600

    zf-zebrachain 0.0.1-pre.0

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@
 //! ZebraChain uses the [NIST post quantum standard][nist.gov] algorithm [ML-DSA] (FIPS-204)
 //! alongside [ed25519] in a hybrid signing construction (as recommended by the ML-DSA authors).
 //!
+//! This is a pre-release crate. The API is still being finalized. A quick-start will be added soon.
+//!
 //! [nist.gov]: https://www.nist.gov/news-events/news/2024/08/nist-releases-first-3-finalized-post-quantum-encryption-standards
 //! [ML-DSA]: https://csrc.nist.gov/pubs/fips/204/final
 //! [ed25519]: https://ed25519.cr.yp.to/
