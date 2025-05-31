15b0dba7824c06ff9d2ecde0d059c840b75fee5f
Jason Gerard DeRose <jasonderose@gmail.com>
2025-05-30 23:42:18 -0600

    Relpace blake3 use in api_test.rs with new Secret.derive_with_foo() stuff

--- tests/api_test.rs
+++ tests/api_test.rs
@@ -2,38 +2,45 @@ use hex_literal::hex;
 use std::collections::HashSet;
 use tempfile;
 use zf_zebrachain::{
-    ChainStore, DIGEST, Hash, MutSecretBlock, OwnedChainStore, PAYLOAD, Payload, Secret,
+    ChainStore, DIGEST, Hash, MutSecretBlock, OwnedChainStore, PAYLOAD, Payload, SECRET, Secret,
     SecretChainStore, Seed,
 };
 
-const SAMPLE_PAYLOAD_0: &str = "d5b8c07e502ecfe37ae644d9bb91ba3f409fff5dbf3856e87a4ec09143f7789d";
-const SAMPLE_PAYLOAD_419: &str = "dc54d0feef9a7986e306947a5e3c31540dea6c473705f79eaf39a6e9dac2c93b";
+const SAMPLE_PAYLOAD_0: &str = "e0e10fc3576f67531060aecd6d8b61a45a1a11e19349c3b6419847c7d5a09ba2";
+const SAMPLE_PAYLOAD_419: &str = "e3db9aa3016c660f7aea654481dc161c1c6be67710ae9223d94eaacb5f730e61";
 
-const BLOCK_HASH_0: &str = "6584f0b69592860651dd3eca0992bc2e0e9271b98bede1f48b5175465dcba02e";
-const BLOCK_HASH_419: &str = "9ef01f3119d356f64f9ce9e1150abdb5ea81d5b8ae4c5ba8f5a96add55ee4221";
+const BLOCK_HASH_0: &str = "3f7f332f2b72b57201b1c5d240c5e05af97bdf919a899e9c4213e02d55c4f70a";
+const BLOCK_HASH_419: &str = "5bf6beac09c722708fdbbe44abf891bc132f33090f596430fc54651f2265ce85";
 
-fn sample_entropy(index: u128) -> Secret {
-    let mut h = blake3::Hasher::new();
-    h.update(
-        b"This will be our bad entropy with random access. Do not do this in real life, haha.",
-    );
-    h.update(&index.to_le_bytes());
-    Secret::from_bytes(*h.finalize().as_bytes())
+static JUNK_ENTROPY: [u8; SECRET] =
+    hex!("4e08e740cad03d0ac8ed4d2d1577b6f48bf6865c0e5c12eeb2082ea95cbda17b");
+static JUNK_STORAGE_SECRET: [u8; SECRET] =
+    hex!("8c793ba2e78be472b42e921dd0d318a5115c1e45c7f9bd2f71b61270cf39b4a4");
+static JUNK_PAYLOAD_HASH: [u8; SECRET] =
+    hex!("7f24701c4590693a7bb12a6353b87aa9c108721753c92cf24b49a65664c521bf");
+static JUNK_PAYLOAD_TIME: [u8; SECRET] =
+    hex!("c34790c3c9e52ab1b166280a5b4493177379c65eaf48f43da0e1d31b79775c82");
+
+fn sample_entropy(index: u64) -> Secret {
+    let root = Secret::from_bytes(JUNK_ENTROPY);
+    root.derive_with_index(index)
 }
 
 #[test]
 fn test_sample_entropy() {
     assert_eq!(
         sample_entropy(0),
-        Secret::from_bytes(hex!(
-            "96b3a086291fbcdef17e52e60731e96d8d36ae0944f2aad0c0c12a0c14e161ca"
-        ))
+        Secret::from_bytes([
+            93, 99, 16, 92, 27, 103, 79, 94, 191, 105, 131, 9, 22, 202, 43, 105, 70, 139, 26, 203,
+            223, 222, 82, 176, 124, 157, 172, 42, 113, 213, 0, 194
+        ])
     );
     assert_eq!(
         sample_entropy(419),
-        Secret::from_bytes(hex!(
-            "27068b40079a37f5ecfb01700135dc9a81d2c811878d2328475dd1724b40891a"
-        ))
+        Secret::from_bytes([
+            251, 254, 81, 169, 131, 113, 14, 45, 220, 27, 201, 193, 179, 94, 16, 233, 153, 65, 58,
+            247, 71, 86, 33, 244, 249, 49, 247, 3, 51, 44, 46, 10
+        ])
     );
     let mut hset: HashSet<Secret> = HashSet::with_capacity(420);
     for index in 0..420 {
@@ -42,28 +49,26 @@ fn test_sample_entropy() {
     assert_eq!(hset.len(), 420);
 }
 
-fn sample_storage_secret(index: u128) -> Secret {
-    let mut h = blake3::Hasher::new();
-    h.update(
-        b"This will a bad sample storage secret with random access. Seriously, do NOT do in real life.",
-    );
-    h.update(&index.to_le_bytes());
-    Secret::from_bytes(*h.finalize().as_bytes())
+fn sample_storage_secret(index: u64) -> Secret {
+    let root = Secret::from_bytes(JUNK_STORAGE_SECRET);
+    root.derive_with_index(index)
 }
 
 #[test]
 fn test_sample_storage_secret() {
     assert_eq!(
         sample_storage_secret(0),
-        Secret::from_bytes(hex!(
-            "28ad9dc97e10d576d16e3e94fe4ef944d3a3215b2aaec67398c70831515f964c"
-        ))
+        Secret::from_bytes([
+            143, 62, 12, 13, 4, 93, 97, 188, 186, 30, 18, 30, 225, 39, 181, 176, 249, 7, 61, 147,
+            106, 81, 117, 51, 176, 132, 21, 53, 63, 25, 37, 229
+        ])
     );
     assert_eq!(
         sample_storage_secret(419),
-        Secret::from_bytes(hex!(
-            "96c7864ce923cec5795c7a5316b961055ef762278e3c0ec9fa3a9a4c9729fd9c"
-        ))
+        Secret::from_bytes([
+            44, 207, 192, 131, 200, 167, 204, 128, 55, 125, 129, 149, 2, 195, 150, 27, 77, 65, 53,
+            202, 33, 160, 94, 186, 68, 39, 96, 93, 53, 48, 39, 117
+        ])
     );
     let mut hset: HashSet<Secret> = HashSet::with_capacity(420);
     for index in 0..420 {
@@ -72,13 +77,11 @@ fn test_sample_storage_secret() {
     assert_eq!(hset.len(), 420);
 }
 
-fn sample_payload(index: u128) -> Payload {
-    let mut h = blake3::Hasher::new();
-    h.update(b"This will be our test payload generator with random access");
-    h.update(&index.to_le_bytes());
-    let root = h.finalize();
-    let time = u128::from_le_bytes(root.as_bytes()[0..16].try_into().unwrap());
-    let state_hash = Hash::compute(root.as_bytes());
+fn sample_payload(index: u64) -> Payload {
+    let root1 = Secret::from_bytes(JUNK_PAYLOAD_HASH).derive_with_index(index);
+    let state_hash = Hash::from_bytes(*root1.as_bytes());
+    let root2 = Secret::from_bytes(JUNK_PAYLOAD_TIME).derive_with_index(index);
+    let time = u128::from_le_bytes(root2.as_bytes()[0..16].try_into().unwrap());
     Payload::new(time, state_hash)
 }
 
